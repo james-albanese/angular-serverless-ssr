e3bf25a07b17412a535e9efe84b4ef742ac745f6
james-albanese <57870271+james-albanese@users.noreply.github.com>
2023-12-03 17:32:35 -0500

    Add StaticFileHandler to server.ts

--- server.ts
+++ server.ts
@@ -1,56 +1,84 @@
-import { APP_BASE_HREF } from '@angular/common';
-import { CommonEngine } from '@angular/ssr';
-import express from 'express';
-import { fileURLToPath } from 'node:url';
-import { dirname, join, resolve } from 'node:path';
-import bootstrap from './src/main.server';
+import { CommonEngine, CommonEngineOptions, CommonEngineRenderOptions } from "@angular/ssr";
+import { APP_BASE_HREF } from "@angular/common";
+import { APIGatewayProxyEventV2, APIGatewayProxyHandlerV2, APIGatewayProxyResultV2 } from "aws-lambda";
+import { fileURLToPath } from "node:url";
+import { dirname, join, resolve, extname } from "node:path";
+import { promises } from "node:fs";
 
-// The Express app is exported so that it can be used by serverless Functions.
-export function app(): express.Express {
-  const server = express();
-  const serverDistFolder = dirname(fileURLToPath(import.meta.url));
-  const browserDistFolder = resolve(serverDistFolder, '../browser');
-  const indexHtml = join(serverDistFolder, 'index.server.html');
+import bootstrap from "./src/main.server";
 
-  const commonEngine = new CommonEngine();
+class StaticFileHandler {
 
-  server.set('view engine', 'html');
-  server.set('views', browserDistFolder);
+  public static isStaticFile(path: string): boolean {
+    if (path.includes(".") && !path.endsWith(".html")) return true;
+    return false;
+  }
 
-  // Example Express Rest API endpoints
-  // server.get('/api/**', (req, res) => { });
-  // Serve static files from /browser
-  server.get('*.*', express.static(browserDistFolder, {
-    maxAge: '1y'
-  }));
+  constructor(private readonly browserDir: string) { }
 
-  // All regular routes use the Angular engine
-  server.get('*', (req, res, next) => {
-    const { protocol, originalUrl, baseUrl, headers } = req;
+  public async handle(path: string): Promise<APIGatewayProxyResultV2> {
+    const filePath = join(this.browserDir, path);
+    const fileExt = extname(filePath);
+    const fileContent = await promises.readFile(filePath);
+    const contentType = this._getContentType(fileExt);
+    return {
+      statusCode: 200,
+      headers: {
+        "Content-Type": contentType
+      },
+      body: fileContent.toString()
+    }
+  }
 
-    commonEngine
-      .render({
-        bootstrap,
-        documentFilePath: indexHtml,
-        url: `${protocol}://${headers.host}${originalUrl}`,
-        publicPath: browserDistFolder,
-        providers: [{ provide: APP_BASE_HREF, useValue: baseUrl }],
-      })
-      .then((html) => res.send(html))
-      .catch((err) => next(err));
-  });
+  private _getContentType(extension: string): string {
+    const mapping: Record<string, string> = {
+      ".js": "text/javascript",
+      ".css": "text/css",
+      // Add more MIME types as necessary
+      // https://developer.mozilla.org/en-US/docs/Web/HTTP/Basics_of_HTTP/MIME_types/Common_types
+    }
+    return mapping[extension] || "application/octet-stream";
+  }
 
-  return server;
 }
 
-function run(): void {
-  const port = process.env['PORT'] || 4000;
+export const handler: APIGatewayProxyHandlerV2 = async (event: APIGatewayProxyEventV2) => {
 
-  // Start up the Node server
-  const server = app();
-  server.listen(port, () => {
-    console.log(`Node Express server listening on http://localhost:${port}`);
-  });
+  const serverDir = dirname(fileURLToPath(import.meta.url));
+  const browserDir = resolve(serverDir, "../browser");
+  const indexHtml = join(serverDir, "index.server.html");
+
+  const { headers, requestContext } = event;
+  const { host } = headers;
+  const { path } = requestContext.http;
+
+  if (StaticFileHandler.isStaticFile(path)) {
+    const staticFileHandler = new StaticFileHandler(browserDir);
+    return await staticFileHandler.handle(path);
+  }
+
+  const commonEngineOptions: CommonEngineOptions = {
+    enablePerformanceProfiler: true
+  }
+
+  const commonEngine = new CommonEngine(commonEngineOptions);
+
+  const renderingOptions: CommonEngineRenderOptions = {
+    bootstrap: bootstrap,
+    documentFilePath: indexHtml,
+    url: `https://${host}${path}`,
+    publicPath: browserDir,
+    providers: [{ provide: APP_BASE_HREF, useValue: "/" }],
+  }
+
+  const result = await commonEngine.render(renderingOptions);
+
+  return {
+    statusCode: 200,
+    headers: {
+      "Content-Type": "text/html"
+    },
+    body: result
+  }
+
 }
-
-run();
