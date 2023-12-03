d53b8b90df7d7ad3700e35f5958a4ecb119c5451
james-albanese <57870271+james-albanese@users.noreply.github.com>
2023-12-03 17:30:54 -0500

    Update app.config.ts with new providers

--- src/app/app.config.ts
+++ src/app/app.config.ts
@@ -1,9 +1,14 @@
-import { ApplicationConfig } from '@angular/core';
-import { provideRouter } from '@angular/router';
+import { ApplicationConfig } from "@angular/core";
+import { provideRouter } from "@angular/router";
 
-import { routes } from './app.routes';
-import { provideClientHydration } from '@angular/platform-browser';
+import { routes } from "./app.routes";
+import { provideClientHydration } from "@angular/platform-browser";
+import { provideHttpClient, withFetch } from "@angular/common/http";
 
 export const appConfig: ApplicationConfig = {
-  providers: [provideRouter(routes), provideClientHydration()]
+  providers: [
+    provideRouter(routes),
+    provideHttpClient(withFetch()),
+    provideClientHydration()
+  ]
 };
